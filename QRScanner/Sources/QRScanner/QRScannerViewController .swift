1c321a0461781a0033c633ed2ec8065b02eaeb49
bejitono <stdemicheli@protonmail.com>
2021-04-18 09:24:19 +0700

    Implements qr scanner view controller.

--- QRScanner/Sources/QRScanner/QRScannerViewController .swift
+++ QRScanner/Sources/QRScanner/QRScannerViewController .swift
@@ -1,62 +1,53 @@
 //
 //  QRScannerViewController.swift
-//  PHWallet
+//  QRScanner
 //
-//  Created by Stefano De Micheli on 17/3/21.
-//  Copyright © 2021 OPN Tech Co., Ltd. All rights reserved.
+//  Created by Stefano on 17/4/21.
 //
 
-import PHUIKit
 import UIKit
 import AVFoundation
 
-final class QRScannerViewController: BaseViewController {
+public protocol QRScannerViewControllerDelegate: AnyObject {
+    func qrScanner(_ qrScanner: QRScannerViewController, didSucceedScanningWithCode code: String)
+    func qrScannerDidFail(_ qrScanner: QRScannerViewController)
+    func qrScannerDidFinish(_ qrScanner: QRScannerViewController)
+}
+
+open class QRScannerViewController: UIViewController {
     
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask { .portrait }
+    public weak var delegate: QRScannerViewControllerDelegate?
+    
+    open override var supportedInterfaceOrientations: UIInterfaceOrientationMask { .portrait }
     
     private var flashIsOn = false
     private let overlayView = QRScannerOverlayView()
     private let scannerView = QRScannerView()
-    private let loadingViewController = PHProgressIndicatorOverlayViewController()
-    private let viewModel: QRScannerViewModelType
     
-    init(viewModel: QRScannerViewModelType) {
-        self.viewModel = viewModel
+    public init() {
         super.init(nibName: nil, bundle: nil)
     }
     
-    required init?(coder: NSCoder) {
+    required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func viewDidLoad() {
+    open override func viewDidLoad() {
         super.viewDidLoad()
         scannerView.delegate = self
         overlayView.delegate = self
-        viewModel.input.set(startHandleScan: showLoadingIndicator)
-        viewModel.input.set(finishedHandleScan: dismissLoadingIndicator)
         checkPermissions()
         setupViews()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
+    open override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         if !scannerView.isRunning { scannerView.startScanning() }
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
+    open override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         if scannerView.isRunning { scannerView.stopScanning() }
-    }
-    
-    func showLoadingIndicator(completion: VoidHandler = nil) {
-        loadingViewController.configure(with: .init(title: Localizable.generalLoadingTitle()))
-        loadingViewController.modalPresentationStyle = .overFullScreen
-        present(loadingViewController, animated: true, completion: completion)
-    }
-    
-    func dismissLoadingIndicator(completion: VoidHandler = nil) {
-        loadingViewController.dismiss(animated: true, completion: completion)
     }
 }
 
@@ -66,12 +57,16 @@ extension QRScannerViewController: QRScannerViewDelegate {
     
     func qrScanner(_ qrScanner: QRScannerView, didSucceedScanningWithCode code: String) {
         scannerView.stopScanning()
-        viewModel.input.handleScan(code: code)
+        delegate?.qrScanner(self, didSucceedScanningWithCode: code)
     }
     
-    func qrScannerDidFail(_ qrScanner: QRScannerView) { }
+    func qrScannerDidFail(_ qrScanner: QRScannerView) {
+        delegate?.qrScannerDidFail(self)
+    }
     
-    func qrScannerDidFinish(_ qrScanner: QRScannerView) { }
+    func qrScannerDidFinish(_ qrScanner: QRScannerView) {
+        delegate?.qrScannerDidFinish(self)
+    }
 }
 
 // MARK: - QRScannerOverlayViewDelegate
@@ -79,7 +74,8 @@ extension QRScannerViewController: QRScannerViewDelegate {
 extension QRScannerViewController: QRScannerOverlayViewDelegate {
     
     func qrOverlayDidPressBack() {
-        viewModel.input.cancel()
+        dismiss(animated: true)
+        delegate?.qrScannerDidFinish(self)
     }
     
     func qrOverlayDidPressFlash() {
@@ -93,7 +89,6 @@ extension QRScannerViewController: QRScannerOverlayViewDelegate {
 private extension QRScannerViewController {
     
     func setupViews() {
-        hideNavigationBar()
         setupQRScannerView()
         setupQROverlayView()
     }
@@ -118,7 +113,7 @@ private extension QRScannerViewController {
             overlayView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             overlayView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
-        overlayView.title = viewModel.output.title
+        overlayView.title = "Place QR code in the frame"
     }
     
     func toggleTorch(turnOn: Bool) {
@@ -143,7 +138,8 @@ private extension QRScannerViewController {
                     if granted {
                         return
                     } else {
-                        self.viewModel.input.cancel()
+                        self.dismiss(animated: true)
+                        self.delegate?.qrScannerDidFinish(self)
                     }
                 }
             })
@@ -154,12 +150,45 @@ private extension QRScannerViewController {
     
     func showNoticeAlert() {
         let alert = makeNoticeAlert(
-            title: viewModel.output.noticeTitle,
-            message: viewModel.output.noticeMessage,
-            actionTitle: viewModel.output.noticeConfirm,
-            cancelTitle: viewModel.output.noticeCancel,
-            cancelAction: { _ in self.viewModel.input.cancel() }
+            title: "Notice",
+            message: "To scan QR code, please allow access to your camera in your device settings.",
+            actionTitle: "Settings",
+            cancelTitle: "Cancel",
+            cancelAction: { _ in
+                self.dismiss(animated: true)
+                self.delegate?.qrScannerDidFinish(self)
+            }
         )
         present(alert, animated: true)
+    }
+    
+    func makeNoticeAlert(
+        title: String,
+        message: String,
+        actionTitle: String,
+        cancelTitle: String,
+        confirmAction: (() -> Void)? = nil,
+        cancelAction: ((UIAlertAction) -> Void)? = nil
+    ) -> UIViewController {
+        let confirmAction = UIAlertAction(
+            title: actionTitle,
+            style: .default,
+            handler: { _ in
+                guard let settingsURL = URL(string: UIApplication.openSettingsURLString),
+                      UIApplication.shared.canOpenURL(settingsURL) else { return }
+                confirmAction?()
+                UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
+            })
+        let cancelAction = UIAlertAction(title: cancelTitle,
+                                         style: .cancel,
+                                         handler: cancelAction)
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        alert.addAction(confirmAction)
+        alert.addAction(cancelAction)
+        return alert
     }
 }
